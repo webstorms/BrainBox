ec8cba476705c324a90d736d0072ec7c5b78d652
Luke Taylor <webstorms@gmail.com>
2021-04-05 11:13:41 +0200

    Added rdm metrics

--- brainbox/metrics/_metrics.py
+++ brainbox/metrics/_metrics.py
@@ -1,4 +1,6 @@
 import torch
+from scipy import stats
+from sklearn.utils import resample
 
 
 def cc(output, target):
@@ -33,5 +35,63 @@ def cc_norm(output, target):
 
     return _cc_norm
 
-# TODO rdm
+
+def rdm(responses, percentile=True):
+    # responses: s x n
+
+    def to_percentile(rdm):
+        dim = rdm.shape[0]
+        vec = rdm.flatten()
+
+        return (stats.rankdata(vec, 'average') / len(vec)).reshape(dim, dim)
+
+    s = responses.shape[0]
+    n = responses.shape[1]
+
+    rdm = torch.zeros((s, s))
+
+    for i in range(s):
+        resp_i = responses[i].view(1, 1, n, 1).repeat(s - i, 1, 1, 1)
+        resp_j = responses[i:].view(s - i, 1, n, 1)
+
+        v = cc(resp_j, resp_i)
+
+        rdm[i, i:] = v[:, 0, 0]
+        rdm[i:, i] = v[:, 0, 0]
+
+    if percentile:
+        rdm = torch.from_numpy(to_percentile(rdm.numpy()))
+
+    return rdm
+
+
+def rdm_deviation(rdm_model, rdm_target, metric='spearman', n_bootstrap=1):
+
+    def flatten_rdm(rdm):
+        s = rdm.shape[0]
+
+        rdm_cells = []
+        for i in range(s):
+            rdm_cells.append(rdm[i, i:].flatten())
+
+        return torch.cat(rdm_cells)
+
+    flatten_rdm_model = flatten_rdm(rdm_model)
+    flatten_rdm_target = flatten_rdm(rdm_target)
+
+    idxs = list(range(len(flatten_rdm_model)))
+
+    deviations = []
+
+    for i in range(n_bootstrap):
+        boot_idxs = resample(idxs, replace=True, n_samples=len(idxs))
+        flatten_rdm_model_boot = flatten_rdm_model[boot_idxs]
+        flatten_rdm_target_boot = flatten_rdm_target[boot_idxs]
+
+        if metric == 'spearman':
+            deviations.append(1 - stats.spearmanr(flatten_rdm_model_boot, flatten_rdm_target_boot)[0])
+        else:
+            raise NotImplementedError
+
+    return deviations
 
