847cb26c31106e7c60137ec26f04cd7e2ae25670
Luke Taylor <webstorms@gmail.com>
2022-06-01 12:42:15 +0100

    Fixed refactor bug

--- brainbox/models/model.py
+++ brainbox/models/model.py
@@ -26,26 +26,26 @@ class BBModel(nn.Module):
                 return name
 
         return None
-
+    
     def init_weight(self, weight, init_type, **kwargs):
         weight_name = self._get_variable_name(weight)
 
         if init_type == "constant":
-            w_initializer = _weight_init.Constant(weight_name, kwargs.get("c", 0.42))
+            w_initializer = weight_init.Constant(weight_name, kwargs.get("c", 0.42))
         elif init_type == "uniform":
-            w_initializer = _weight_init.Uniform(
+            w_initializer = weight_init.Uniform(
                 weight_name, kwargs.get("a", 0), kwargs.get("b", 1)
             )
         elif init_type == "glorot_uniform":
-            w_initializer = _weight_init.GlorotUniform(weight_name)
+            w_initializer = weight_init.GlorotUniform(weight_name)
         elif init_type == "normal":
-            w_initializer = _weight_init.Normal(
+            w_initializer = weight_init.Normal(
                 weight_name, kwargs.get("mean", 0), kwargs.get("std", 1)
             )
         elif init_type == "glorot_normal":
-            w_initializer = _weight_init.GlorotNormal(weight_name)
+            w_initializer = weight_init.GlorotNormal(weight_name)
         elif init_type == "identity":
-            w_initializer = _weight_init.Identity(weight_name, kwargs.get("c", 1))
+            w_initializer = weight_init.Identity(weight_name, kwargs.get("c", 1))
 
         w_initializer(weight)
 
