6ff39b3db8bcfb7f880235f7762d3ac630dbf515
Luke Taylor <webstorms@gmail.com>
2022-06-09 08:44:32 +0100

    Added patched dataset

--- brainbox/datasets/base.py
+++ brainbox/datasets/base.py
@@ -157,9 +157,54 @@ class PredictionTemporalDataset(TemporalDataset):
         clip = self.load_clip(i)
 
         x = clip[:, : -self._pred_horizon]
-        y = clip[:, self._pred_horizon :]
+        y = clip[:, self._pred_horizon:]
 
         return x, y
 
     def load_clip(self, i):
         raise NotImplementedError
+
+
+class PatchPredictionTemporalDataset(PredictionTemporalDataset):
+
+    def __init__(self, root, sample_length, dt, in_k, out_k, stride, n_clips, clip_length, clip_h, clip_w, pred_horizon, train=True, preprocess=None, transform=None, target_transform=None, push_gpu=False):
+        super().__init__(root, sample_length, dt, n_clips, clip_length, pred_horizon, train, preprocess, transform, target_transform, push_gpu)
+
+        self._in_k = in_k
+        self._out_k = out_k
+        self._stride = stride
+
+        self._clip_h = clip_h
+        self._clip_w = clip_w
+
+        self._ids = []
+        for clip_id in range(n_clips):
+            for t_id in range(0, clip_length - sample_length - pred_horizon + 1, dt):
+                for h_id in range(0, clip_h, self._stride):
+                    for w_id in range(0, clip_w, self._stride):
+                        if h_id + in_k <= clip_h and w_id + in_k <= clip_w:
+                            self._ids.append((clip_id, t_id, h_id, w_id))
+
+    @property
+    def hyperparams(self):
+        return {**super().hyperparams, "in_k": self._in_k, "out_k": self._out_k, "stride": self._stride, "clip_h": self._clip_h, "clip_w": self._clip_w}
+
+    def __getitem__(self, i):
+        clip_id, t_id, h_id, w_id = self._ids[i]
+        x, y = self.load_clips(clip_id)
+        p = int((self._in_k - self._out_k) / 2)
+        x = x[:, t_id : t_id + self._sample_length, h_id:h_id+self._in_k, w_id:w_id+self._in_k]
+        y = y[:, t_id : t_id + self._sample_length, h_id+p:h_id+self._in_k-p, w_id+p:w_id+self._in_k-p]
+
+        if self._transform is not None:
+            x = self._transform(x)
+
+        if self._target_transform is not None:
+            y = self._target_transform(y)
+
+        return x, y
+
+
+
+
+
