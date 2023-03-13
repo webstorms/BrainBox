6167c7d03a21e50b43eeb55b9722b56b50185bcd
Luke Taylor <webstorms@gmail.com>
2023-03-13 14:48:29 +0000

    More metrics: SPE and computing RDM deviation without boostrap

--- brainbox/physiology/neural/correlation.py
+++ brainbox/physiology/neural/correlation.py
@@ -16,17 +16,28 @@ def cc(output, target):
     _cc = (exp_xy - exp_x * exp_y) / (
             (torch.sqrt(exp_x2 - exp_x**2)) * (torch.sqrt(exp_y2 - exp_y**2))
     )
-    
+
     return _cc
 
 
-def compute_max_cc(target):
+def compute_max_cc(target, eps=1e-3, unbiased=True):
     # target: t x n
     assert len(target.shape) == 2
 
     n = target.shape[1]
-    np = (n - 1) * target.var(dim=0).sum()
-    sp = target.sum(dim=1).var() - target.var(dim=0).sum()
+    np = (n - 1) * target.var(dim=0, unbiased=unbiased).sum()
+    sp = target.sum(dim=1).var(unbiased=unbiased) - target.var(dim=0, unbiased=unbiased).sum()
     max_cc2 = 1 + 1 / n * (np / sp)
 
-    return 1 / torch.sqrt(max_cc2) if max_cc2 > 0 else 1
+    return 1 / torch.sqrt(max_cc2) if max_cc2 > 0 else eps
+
+
+def compute_max_spe(target, unbiased=True):
+    # target: t x n
+    assert len(target.shape) == 2
+
+    n = target.shape[1]
+    sp = (1 / (n - 1)) * (n * target.mean(dim=1).var(unbiased=unbiased) - target.var(dim=0, unbiased=unbiased).mean())
+    rp = target.mean(1).var(unbiased=unbiased)
+
+    return sp / rp
